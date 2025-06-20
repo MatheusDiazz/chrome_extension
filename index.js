47e2083b7cd506fb6b3556c96f79928b587a5796
Matheus Diaz <matheusdz155@gmail.com>
2025-06-20 11:13:38 -0400

    user input is saved and added to a string, each input is saved as a clickable link that opens a new tab

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-myLeads = []
+let myLeads = []
 const inputEl = document.getElementById("input-el")
-const  inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("un-list")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    inputEl.focus()
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.textContent += myLeads[i] + " "
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`
+    }
+    ulEl.innerHTML = listItems
 }
+
+
+
 
