376ccd8d5f9d2ca80657203ae0a71668c7391338
Matheus Diaz <matheusdz155@gmail.com>
2025-06-21 16:54:32 -0400

    version ready for deployment

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-const deleteBtn = document.getElementById("delete-btn")
-const saveTabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
-
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/matheusfernandodiazlacerda/"}
-]
-
-saveTabBtn.addEventListener("click", function() {
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    if (tabs[0] && tabs[0].url) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    } else {
+        alert("Could not get the current tab's URL.")
+    }
+    })
 })
 
-deleteBtn.addEventListener("click", function() {
-    localStorage.removeItem('myLeads')
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
     myLeads = []
     render(myLeads)
 })
@@ -31,22 +46,6 @@ deleteBtn.addEventListener("click", function() {
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    inputEl.focus()
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
-
 })
-
-function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-    listItems += `
-        <li>
-            <a target='_blank' href='${leads[i]}'>
-                ${leads[i]}
-            </a>
-        </li>
-    `
-    }
-    ulEl.innerHTML = listItems
-}
