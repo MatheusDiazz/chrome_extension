645f010cd5301a01622da27e77196c59350e29d5
Matheus Diaz <matheusdz155@gmail.com>
2025-06-20 21:32:44 -0400

    user input gets stored in browser's local storage and memory is persistent

--- index.js
+++ index.js
@@ -3,33 +3,38 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     inputEl.focus()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+
 })
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-      //  listItems += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`
-        listItems = ""
-        for (let i = 0; i < myLeads.length; i++) {
-            listItems += `
-                <li>
-                    <a target='_blank' href='${myLeads[i]}'>
-                        ${myLeads[i]}
-                    </a>
-                </li>
-                
-            `
+    listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
 
-        }
-        
+    `
     }
     ulEl.innerHTML = listItems
 }
+
 
 
 
